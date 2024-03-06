edfe57a32b814c1eaaeca3270e7765c7814178ab
rashidgh <lumiarashid5200@gmail.com>
2024-03-06 23:31:06 +0530

    updated

--- src/app/components/updatedata/page.jsx
+++ src/app/components/updatedata/page.jsx
@@ -1,45 +1,52 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const CreateData = () => {
   let [data, setData] = useState();
+  console.log(data);
   let [state, setState] = useState({
     email: data?.email,
     password: data?.password,
   });
-  let { email, password } = state;
-  let id=window.localStorage.getItem("id");
 
-   let getData = async () => {
+  let { email, password } = state;
+  // console.log(state);
+  let id = window.localStorage.getItem("id");
+
+  let getData = async () => {
     const response = await axios.get(`http://localhost:3001/data/${id}`);
     // console.log(response?.data);
     setData(response?.data);
     window.localStorage.setItem("id", response?.data?.id);
   };
-  useEffect(()=>{
-    getData()
-  },[])
-  console.log(data);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    let handleUpdate=async(e)=>{
-      e.preventDefault();
-      let payload = {
-        email:email,
-        password:password
-      }
-    let data=await axios.put(`http://localhost:3001/data/${id}`,payload);
-      console.log(data);
-      setData({
-        email:"",
-        password:""
-      })
+  let handleUpdate = async e => {
+    e.preventDefault();
+    let payload = {
+      email: email,
+      password: password,
+    };
+    let data = await axios.put(`http://localhost:3001/data/${id}`, payload);
+    console.log(data);
+    setData({
+      email: "",
+      password: "",
+    });
+    if (data?.status == 200) {
+      toast.success("updated successfully");
+      window.location.assign("/")
+    }
   };
 
-  let handleChange=(e)=>{
-    let{name,value}=e.target;
-    setState({...state,[name]:value});
-  }
+  let handleChange = e => {
+    let { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
 
   return (
     <section className="w-[100vw] h-[100vh]">
@@ -51,8 +58,8 @@ const CreateData = () => {
               type="text"
               className="p-3 w-[25vw]"
               name="email"
-                value={email}
-                onChange={handleChange}
+              value={email}
+              onChange={handleChange}
               placeholder="Enter email here"
             />
           </div>
@@ -61,13 +68,18 @@ const CreateData = () => {
               type="password"
               className="p-3 w-[25vw]"
               name="password"
-                value={password}
-                onChange={handleChange}
+              value={password}
+              onChange={handleChange}
               placeholder="Enter password here"
             />
           </div>
           <div>
-            <button className="btn  btn-info w-full text-base" onClick={handleUpdate}>Update</button>
+            <button
+              className="btn  btn-info w-full text-base"
+              onClick={handleUpdate}
+            >
+              Update
+            </button>
           </div>
         </form>
       </div>
